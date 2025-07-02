91700f80f91f138b265198c82c781c19fba35cea
Shubham <shubhamyadav97660@gmail.com>
2025-07-02 14:06:42 +0530

    used .env for mongo password

--- server/config.js
+++ server/config.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const connectDB = () => mongoose.connect("mongodb+srv://shubhamyadav97660:qeJWf2uGkiyiTFbw@cluster0.pqhsdnv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+const connectDB = () => mongoose.connect(`mongodb+srv://shubhamyadav97660:${process.env.MongoPassword}@cluster0.pqhsdnv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
     .then(() => console.log("Connected To MongoDB"))
     .catch(err => console.log(err.message))
 
