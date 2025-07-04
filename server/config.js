ebd6c6a0e121408906081d809c2aedf5adc97d98
Shubham <shubhamyadav97660@gmail.com>
2025-07-04 14:22:42 +0530

    server port changed to process.env.PORT

--- server/config.js
+++ server/config.js
@@ -5,4 +5,10 @@ const connectDB = () => mongoose.connect(`mongodb+srv://shubhamyadav97660:${proc
     .then(() => console.log("Connected To MongoDB"))
     .catch(err => console.log(err.message))
 
+// const connectDB = () => mongoose.connect("mongodb://localhost:27017/business")
+//     .then(() => console.log("Connected To MongoDB"))
+//     .catch(err => console.log(err.message))
+
+
+
 module.exports = connectDB;
--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use("/furniture", furnitureRoutes);
